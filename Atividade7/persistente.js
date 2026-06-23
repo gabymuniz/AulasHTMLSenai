422c4fe908f284da148d745e5194b5d4b7af5b85
Gabriela Muniz <54966461+gabymuniz@users.noreply.github.com>
2026-06-23 00:50:18 +0000

    Atividade 7

---
+++ Atividade7/persistente.js
@@ -0,0 +1,17 @@
+function salvarNome(){
+    let valorDigitado = document.getElementById("texto-inserido").value;
+    let idadeDigitado = document.getElementById("idade-inserido").value;
+
+    let objetoPessoa = {
+        nome: valorDigitado,
+        idade: idadeDigitado
+
+
+
+    };
+    let textoJSON = JSON.stringify(objetoPessoa)
+    localStorage.setItem("dadoNome",textoJSON)
+
+    document.getElementById("nome-salvo").innerText = valorDigitado;
+    document.getElementById("idade-salvo").innerText = idadeDigitado;
+}
