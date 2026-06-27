8c99ecb4bdf2a4d576c7a5369da5691c768fb234
Gabriela Muniz <54966461+gabymuniz@users.noreply.github.com>
2026-06-27 00:46:10 +0000

    Buscador de Cep

---
+++ Atividade9/js/buscaCep.js
@@ -0,0 +1,45 @@
+function buscarCep(){
+ 
+    let cepDigitado = document.getElementById("cep").value;
+
+    if(cepDigitado === ""){
+        return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`)
+
+        .then(function(resposta){
+
+            return resposta.json();
+
+        })
+
+        .then(function(dados){
+
+            if(dados.erro){
+                alert("CEP não encontrado!")
+            }
+
+            document.getElementById("rua").value = dados.logradouro;
+            
+            document.getElementById("bairro").value = dados.bairro;
+
+            document.getElementById("cidade").value = dados.localidade;
+
+            document.getElementById("uf").value = dados.uf;
+
+            
+
+        })
+         .catch(function(erro) {
+        console.log("Erro:", erro);
+         })
+}
+
+const campoCep = document.getElementById("cep");
+campoCep.addEventListener('focusout', buscarCep)
+    
+    
+
+
+    
