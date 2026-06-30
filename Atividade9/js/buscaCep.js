9bc45b897c3c2080b575619e892899ffb8e3376b
Gabriela Muniz <54966461+gabymuniz@users.noreply.github.com>
2026-06-30 22:41:51 +0000

    Adicionada validação de CEP

--- Atividade9/js/buscaCep.js
+++ Atividade9/js/buscaCep.js
@@ -1,45 +1,54 @@
-function buscarCep(){
- 
+function buscarCep() {
+
     let cepDigitado = document.getElementById("cep").value;
 
-    if(cepDigitado === ""){
+    const cep = cepDigitado;
+
+    const cepCorreto = cep.replace(/-/g, ""); // faz com que todas as vezes que aparecer um -, o texto seja substituido por "";
+
+    console.log(cepCorreto);
+
+    if (cepCorreto === "" || cepCorreto.length !== 8) {
+
+        window.alert("CEP INVÁLIDO!!");
         return;
+        
     }
+    
+    fetch(`https://viacep.com.br/ws/${cepCorreto}/json/`)
 
-    fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`)
-
-        .then(function(resposta){
+        .then(function (resposta) {
 
             return resposta.json();
 
         })
 
-        .then(function(dados){
+        .then(function (dados) {
 
-            if(dados.erro){
+            if (dados.erro) {
                 alert("CEP não encontrado!")
             }
 
             document.getElementById("rua").value = dados.logradouro;
-            
+
             document.getElementById("bairro").value = dados.bairro;
 
             document.getElementById("cidade").value = dados.localidade;
 
             document.getElementById("uf").value = dados.uf;
 
-            
+
 
         })
-         .catch(function(erro) {
-        console.log("Erro:", erro);
-         })
+        .catch(function (erro) {
+            console.log("Erro:", erro);
+        })
 }
 
 const campoCep = document.getElementById("cep");
 campoCep.addEventListener('focusout', buscarCep)
-    
-    
 
 
-    
+
+
+
