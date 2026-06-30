8358fc3ad6df8fd32f81ec651227c7e9da9a37df
Gabriela Muniz <54966461+gabymuniz@users.noreply.github.com>
2026-06-30 23:11:26 +0000

    Alterações na Atividade 10

--- Atividade10/js/tratamentoDeErro.js
+++ Atividade10/js/tratamentoDeErro.js
@@ -1,33 +1,28 @@
-function tratamentoDeErro() {
-
-    try{
-        //Estamos executando uma função
+function tratamentoDeErroGenerico(){
+    try {
+        // Estamos executando uma função
         console.log("Iniciando meu bloco Try")
 
-        //Declaramos uma função falsa para ter um erro proposital
+        // Declaramos uma função falsa pra ter um erro de propósito
         funcaoNaoDefinida()
 
-        console.log("Essa linha não vai ser executada")
-    } catch (error){
+        console.log("Essa linha, não vai ser executada")
+    } catch (error) {
         console.error("Erro encontrado", error.message)
     }
 }
 
-function tratamentoDeErroGenerico() {
-
-}
-
-function tratamentoDeErroEspecifico() {
+function tratamentoDeErroEspecifico(){
     try {
-        let idade = -10;
+        let idade = 10;
 
-        if (idade < 0) {
-            throw new Error("Idade inválida! Não pode ter uma idade menor que 0!");
-        } else{
-            throw new Error("Idade inválida! Menor que 18 anos");
+        if(idade < 0) {
+            throw new Error("Idade inválida! Não pode ter uma idade menor que 0")
+        } else {
+            throw new Error("Idade inválida! Menor do que 18 anos")
         }
-
-    } catch(error){
+        
+    } catch (error) {
         console.log("Erro ao cadastrar: ", error.message)
     }
 }
